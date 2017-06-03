a3b98c51559a4f48e049be014c4464e4574a9b91
GTANAdam <adam@gtanet.work>
2017-06-04 01:44:41 +0300

    Attempt to fix slow motion issue

--- Client/Properties/AssemblyInfo.cs
+++ Client/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.521.35")]
-[assembly: AssemblyFileVersion("0.1.521.35")]
+[assembly: AssemblyVersion("0.1.519.349")]
+[assembly: AssemblyFileVersion("0.1.519.349")]
